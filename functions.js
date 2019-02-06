7dae3f03e5d0e0b6cbf2d57aa6a3ba4a72a2791d
Henry McCreery <henrymccy@gmail.com>
2019-02-05 21:02:40 -0800

    added random color

--- functions.js
+++ functions.js
@@ -1,3 +1,12 @@
 function sayHello(){
     alert("hello");
-  }
+    }
+
+function randomColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";  
+    document.body.style.background = bgColor;
+    document.body.style.color = bgcolor;
+    }

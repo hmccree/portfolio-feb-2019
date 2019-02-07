c512f664e1b7ea2bb7d03c8956fc51c9b57cfc4d
Henry McCreery <henrymccy@gmail.com>
2019-02-07 13:40:24 -0800

    many changes

--- functions.js
+++ functions.js
@@ -6,7 +6,7 @@ function randomColor() {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";  
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
     document.body.style.background = bgColor;
-    document.body.style.color = bgcolor;
+    document.getElementById("sidenav").style.background = bgColor;
     }
